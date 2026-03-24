4ce45ed67b36432cb4f80936c86f9b39772f5b49
Yevhen_Shpatakov <yevhen.shpatakov@gmail.com>
2026-03-24 19:46:07 +0100

    3d week

---
+++ rsp_main_project/database/01-init_database.sql
@@ -0,0 +1,11 @@
+-- EXAMPLE 
+-- TODO
+CREATE TABLE sensor (
+    uuid UUID PRIMARY KEY,
+    name TEXT NOT NULL,
+    type TEXT,
+    sensor TEXT,
+    apidetails TEXT,
+    is_online BOOLEAN DEFAULT FALSE
+);
+
